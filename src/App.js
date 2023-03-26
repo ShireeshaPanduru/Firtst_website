474e23b6135ef57e039b9dbff5cfb69ac8cd5172
ShireeshaPanduru <121652807+ShireeshaPanduru@users.noreply.github.com>
2023-03-26 12:08:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MyProfessionalWebsite from './Components/My_pro_web';
+function App() {
+  return (
+    <div className="App">
+    <MyProfessionalWebsite/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/My_pro_web.js
@@ -0,0 +1,261 @@
+import React from 'react'
+import './My_pro_web.css'
+
+import{useState,useEffect}from 'react'
+
+const MyProfessionalWebsite = () => {
+  return (
+    <div>
+            <div id="main">
+            <header id="main_header" >
+                       <img id = "img1"  src="./images/logo.png"/>
+                            <div id="homm" >
+                                      <ul id="ul7">
+                                      <li><a href="#">HOME</a></li>
+                                      <li><a href="#">ABOUT</a></li>
+                                      <li><a href="#">MENU</a></li>
+                                      <li><a href="#">GALLERY</a></li>
+                                      <li><a href="#">CATERING</a></li>
+                                      <li><a href="#">CONTACT</a></li>
+                                     <img  id="img2"  src="../images/sale11.png"/>  
+                                      
+                                  
+                                      </ul>
+                              </div> 
+                                            
+                </header>
+                </div>
+                                  <section id="main_section" >
+                                    {/* <Imgslidee/> */}
+                                 <img  id="img3"  src="../images/slider11.png"/> 
+                            </section>  
+                            <div>
+                                <section id="main_section1"> 
+                                   <p id="p1" >TAKE YOUR<br></br><span id="s1" > PICK</span></p>
+                                    <div id="sc" >
+                                  <div id="dd1">
+                                  <a href="#"><img  id="img4"  src="./images/category_q6piui.jpg"/>
+						                      </a>
+                                  <p id="p2" ><a href="#">SCOOPS</a></p>
+                                  <div id="d1"></div>
+                                                
+                                  </div>
+                           </div>
+
+
+                            <div id="dd2" >
+                                  <a href="#"><img  id="img5"  src="../images/category_q6piuq.jpg"/>
+                                  </a>
+                                  <p id="p3" ><a href="#">ICE CREAM <br></br>CREATIONS</a></p>
+                                  <div id="d2" ></div>
+                           </div>
+
+                           <div id="dd3" >
+                                <a href="#"><img  id="img6" c src="../images/category_qpri7c.jpg"/>
+                                </a>
+                                <p id="p4" ><a href="#">SUNDAES</a></p>
+                                <div id="d3" ></div>
+                          </div>
+
+                           <div id="dd4" >
+                                <a href="#"><img  id="img7"  src="../images/cake.png"/>
+                                </a>
+                                <p id="p5" ><a href="#">ICE CREAM CAKES</a></p>
+                                <div id="d4"></div>
+                          </div>
+                          <div id="sco" >
+                          <div id="dd5" >
+                                        <a href="#"><img  id="img8"  src="../images/category_q6pivh.jpg"/>
+                                        </a>
+                                        <p id="p6"><a href="#">BELGIAN WAFFLES</a></p>
+                                        <div id="d5" ></div>
+                                  </div>
+                                   <div id="dd6" >
+                                        <a href="#"><img  id="img9"  src="../images/category_qpriil.jpg"/>
+                                        </a>
+                                        <p id="p7" ><a href="#">THICK-SHAKES</a></p>
+                                        <div id="d6" ></div>
+                                  </div>
+                                  <div id="dd7">
+                                        <a href="#"><img  id="img10" src="../images/category_qpri83.jpg"/>
+                                        </a>
+                                        <p id="p8" ><a href="#">PREMIUM  PRODUCTS</a></p>
+                                        <div id="d7" ></div>
+                                  </div>
+                                 <div id="dd8" >
+                                          <a href="#"><img  id="img111"  src="../images/images%20(1).jpg"/>
+                                          </a>
+                                          <p id="p9" ><a href="#">ICE CAKES</a></p>
+                                          <div id="d8" ></div>
+                                  </div>
+                                  <a href="#"><div id="view" >
+                                    <p id="al" >
+                                      <a href="#">VIEW ALL</a>
+                                      </p>
+                                  </div></a> 
+                                  
+                          </div>
+                      <div id="our" >
+                          <p id="sign" >OUR SIGNATURE <br></br><span id="cr">CREATIONS</span></p>
+                           <div id="dd9" >
+                                        <a href="#"><img  id="img12"  src="../images/1.png"/>
+                                        </a>
+                                        <div id="d9"></div>
+                                        <a href="#"><img  id="img13"  src="../images/2.png"/>
+                                        </a>
+                                        <div id="d10" ></div>
+                                        <a href="#"><img  id="img14" src="../images/3.png"/>
+                                        </a>
+                                        <div id="d11" ></div>
+                                         <div class="ice"><a href="#"><img  id="img15" class="img15" src="../images/4.png"/>
+						                            </a></div>
+                                       <a href="#"><div id="vie" class="vie"><p id="a1l" class="al"><a href="#">VIEW ALL</a></p></div></a> 
+						
+	
+                                  </div> 
+                          </div>    
+
+                               </section> 
+                              <aside id="aside">
+                              <img id="img16" src="../images/ttm.png"/>
+                              <div id="lead" >
+					            <p id="le" > our leaders share their views on </p>
+                      <p id="lee" >tackling today’s challenges,</p>
+                      <p id="leee" > business expansion plans, </p>
+                      <p id="lea"> and more... </p>
+                      </div>
+                      <p id="p166" ><a href="#">Never has our purpose been more relevant </a></p> 
+                      <p id="p161" ><a href="#">as it is right now. In today’s  fast-moving</a></p>   
+                      <p  id="p162" ><a href="#">business, social and environmental landscape,</a></p>
+                      <p  id="p163" ><a href="#">our leaders share how we’re navigating with</a></p>
+                      <p  id="p164" ><a href="#">action, to create a future we can all be proud of.</a></p>
+
+                      <a href="#"><div id="viee" class="viee"><p id="a1ll" ><a href="#">Hear from our Leaders</a></p></div></a>
+					           
+
+
+                      </aside> 
+                      <aside id="aside1" >
+                      <div id="cream" >
+                      <p id="crm" >CREAM & FUDGE<br></br><span id="fran" class="fran">FRANCHISE BENEFITS</span></p>
+                      <div id="ddd9" >
+                      <a href="#"><img  id="img17" src="../images/franchisee_benefits_q6pigq.jpg"/>
+						                </a>
+                                  <div id="low" >
+                                    <img id="inv" src="../images/franchisee_benefits_q6pikj.jpg"/>
+                                    <p id="invest" ><a href="#">Low Investment</a></p>
+
+                                  </div>
+                                  <div id="hig">
+                                          <img id="high" src="../images/franchisee_benefits_q6pikj1.jpg"/>
+                                          <p id="turn" ><a href="#">High Return</a></p>
+                                      </div>
+						
+                              <div id="full" >
+                                <img id="pro" src="../images/franchisee_benefits_q6pikj2.jpg"/>
+                                <p id="sup" ><a href="#">
+                                  Full Project Support</a></p>
+                              </div>
+						
+                        <div id="tra" >
+                          <img id="trai" src="../images/franchisee_benefits_q6pikj3.jpg"/>
+                          <p id="train" ><a href="#">Training</a></p>
+                        </div>
+                        <div id="mar">
+                              <img id="market" src="../images/franchisee_benefits_q6pikj4.jpg"/>
+                              <p id="supp" ><a href="#">
+                                
+                              Marketing Support</a></p>
+                      </div>
+						
+                    <div id="qua" >
+                      <img id="quali" src="../images/franchisee_benefits_q6pikj5.jpg"/>
+                      <p id="quality" ><a href="#">
+                      Quality Maintaining</a></p>
+
+                    </div>
+                    <p id="fr" ><a href="#">From our Global Service Center based in </a></p> 
+                    <p id="th" ><a href="#"> Thailand, we manage a comprehensive </a></p>   
+                    <p  id="qu"><a href="#">Quality Management System overseeing the</a></p>
+                    <p  id="en" ><a href="#">entire value chain, complete with interior</a></p>
+                    <p  id="st" ><a href="#">style design and graphics team, multimedia </a></p>
+                    <p  id="pr" ><a href="#">production facilities, research & development, </a></p>
+                    <p  id="gl" ><a href="#">global quality management & data and a training  </a></p>
+                    <p  id="ac" ><a href="#">academy. Every aspect has been developed and </a></p>
+                    <p  id="re"><a href="#">refined to achieve our quality mission : the  </a></p>
+                    <p  id="co" ><a href="#">continuous improvement of the customer experie-</a></p>
+                    <p  id="at" ><a href="#">-nce at all stores under each brand worldwide.</a></p>
+
+                    <a href="#"><div id="get" ><p id="el" ><a href="#">GET FRANCHISE QUOTE</a></p></div></a>
+
+                      </div>
+                      </div>
+                      </aside>
+                       <aside id="aside2" >
+                  <div id="ream" >
+                   <p id="test" >TESTIMONIALS</p> 
+                   <img id="child" src="../images/child.png"/> 
+                  
+                  </div>
+                </aside>   
+                 <footer id="footer1" >
+                <img id="foot"   src="./images/logo.png"/>
+                      <div id="motors" ><p id="wv" >About</p></div>
+                      <div id="wheels" ><p id="ww" >Contact Us</p></div>
+                       <div id="customer" ><p  id="xx" >Menu Bar</p></div>
+                      <div id="custo" ><p  id="xx" > Quick Links</p></div>  
+
+
+                      <div id="ul1" >
+                      <ul id="ul44" >
+                            <li><a href="#">Gift Cards</a></li>
+                            <li><a href="#">Blogs</a></li>
+                            <li><a href="#">Career</a></li>
+                            
+                          </ul>
+		                  </div>
+                     <div id="ul3" >
+                          <ul id="ul4" >
+                            <li><a href="#">Cards</a></li>  
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Locator</a></li>
+                            
+                          </ul>
+		            	    </div>
+                       <div id="ul5" >
+                          <ul id="ul6" >
+                            <li><a href="#">Franchisee</a></li>
+                            <li><a href="#">Sustainability</a></li>
+                            <li><a href="#">Store locator</a></li>
+                          </ul>
+                      </div>
+                <div id="ul55" >
+                <ul id="ul66">
+                  <li><a href="#">Our Flavours</a></li>
+                  <li><a href="#">Our Brand Story</a></li>
+                  <li><a href="#">Join Us</a></li>
+                </ul>
+                </div>
+
+                 <div id="fl"></div>
+                <img id="mail"  src="../images/ma1.png"/>
+                <div id="call" >
+			          <img id="cal"  src="../images/call%201%20copy.png"/></div>
+                <div id="fbb" >
+		        	<img id="fb"  src="../images/fb1.png"/></div>
+              <p id="ic" >© 2022,Cream & Fudge Ice Creams</p> 
+
+                </footer> 
+                               
+
+
+                              
+
+
+                </div>
+       
+       </div>
+    
+  )
+  }
+export default MyProfessionalWebsite
